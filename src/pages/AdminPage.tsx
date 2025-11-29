4b60b00abef35555f4ecf8815e48313cca7de4a2
Oh YoungJe <139232765+GulSauce@users.noreply.github.com>
2025-11-29 21:28:01 +0900

    [CSE-23] 관리자 엔드포인트로 변경

--- src/pages/AdminPage.tsx
+++ src/pages/AdminPage.tsx
@@ -194,7 +194,7 @@ export const AdminPage = () => {
     };
 
     try {
-      await axios.post('/draw-point/join', payload);
+      await axios.post('/admin/draw/join', payload);
 
       console.log('Points credited:', payload);
       alert('포인트 적립 완료!');
